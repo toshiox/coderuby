d4a63c080f5470a0a2910ddab714628469632ee6
Gustavo watanabe <gustavo.watanabe@vidibr.com>
2023-11-06 10:36:14 -0300

    fix ItemsController

--- app/app.rb
+++ app/app.rb
@@ -1,9 +1,9 @@
 require 'sinatra'
-# require_relative './controllers/items_controller'
+require_relative './controllers/items_controller'
 require_relative './controllers/article_controller'
 
 configure do
 end
 
-# use ItemsController
+use ItemsController
 use ArticleController
